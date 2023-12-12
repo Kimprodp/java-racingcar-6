98871aa23c8991aa6d2ba1049ea8ac007fb66106
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-12 17:46:29 +0900

    feat(OutputView): 출력 클래스 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,26 @@
+package racingcar.view;
+
+import java.util.LinkedHashMap;
+import racingcar.view.constants.Message;
+
+public class OutputView {
+
+    public static void printMessage(Message message) {
+        System.out.println(message.getMessage());
+    }
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+
+    public static void printRoundResult(LinkedHashMap<String, Integer> roundResult) {
+        for (String car : roundResult.keySet()) {
+            System.out.print(String.format(Message.OUTPUT_ROUND_RESULT.getMessage(), car));
+            for (int i = 0; i < roundResult.get(car); i++) {
+                System.out.print(Message.OUTPUT_FORWARD_RESULT.getMessage());
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
