f3a9649cac2082b835c58bab22af2ac07ac5056b
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-12 18:11:36 +0900

    feat(RacingGame): 우승자를 반환하는 기능 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,7 +1,9 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.view.constants.Message;
 
 public class RacingGame {
@@ -9,21 +11,30 @@ public class RacingGame {
     private List<Car> cars;
     private int numberOfRaces;
     private RacingHistory racingHistory;
+    private List<Car> winner;
 
     public RacingGame(List<Car> cars, int numberOfRaces) {
         this.cars = cars;
         this.numberOfRaces = validateNumberOfRaces(numberOfRaces);
         this.racingHistory = new RacingHistory();
+        this.winner = new ArrayList<>();
     }
 
     public void startRacing() {
         for (int i = 1; i <= numberOfRaces; i++) {
             racingHistory.addHistory(i, runRound());
         }
+        confirmWinner();
     }
 
     public RacingHistory getRacingHistory() {
         return racingHistory;
+    }
+
+    public List<String> getWinnerName() {
+        return winner.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 
     private int validateNumberOfRaces(int numberOfRaces) {
@@ -40,5 +51,16 @@ public class RacingGame {
             roundResult.put(car.getName(), car.getPosition());
         }
         return roundResult;
+    }
+
+    private void confirmWinner() {
+        int highPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        winner.addAll(cars.stream()
+                .filter(car -> car.getPosition() == highPosition)
+                .collect(Collectors.toList()));
     }
 }
