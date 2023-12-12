a4e24ccc927a9fce7904d3e4522410d911d28d41
Gyutae Kim <kiimwf0819@gmail.com>
2023-12-12 17:45:15 +0900

    feat(InputProcessor): 입력값에 대한 검증을 수행하는 클래스 구현

---
+++ src/main/java/racingcar/controller/InputProcessor.java
@@ -0,0 +1,39 @@
+package racingcar.controller;
+
+import racingcar.view.constants.Message;
+
+public class InputProcessor {
+
+    private static final String COMMA = ",";
+
+    public static String[] validateText(String inputValue) {
+        validateEmpty(inputValue);
+        return validateSeparate(inputValue);
+    }
+
+    public static int validateNumber(String inputValue) {
+        validateEmpty(inputValue);
+        return validateInteger(inputValue);
+    }
+
+    private static void validateEmpty(String inputValue) {
+        if (inputValue.isEmpty()) {
+            throw  new IllegalArgumentException(Message.ERROR_EMPTY.getErrorMessage());
+        }
+    }
+
+    private static String[] validateSeparate(String inputValue) {
+        if (!inputValue.contains(COMMA)) {
+            throw new IllegalArgumentException(Message.ERROR_NOT_SEPARATE.getErrorMessage());
+        }
+        return inputValue.split(COMMA);
+    }
+
+    private static int validateInteger(String inputValue) {
+        try {
+            return Integer.parseInt(inputValue);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(Message.ERROR_NOT_INTEGER.getErrorMessage());
+        }
+    }
+}
